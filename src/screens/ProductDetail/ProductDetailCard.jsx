3a10ac922247abfc6e9b522168ea9f1618e9b5e7
MahdiarShirzad <101416335+MahdiarShirzad@users.noreply.github.com>
2024-01-01 14:16:44 +0330

    product detail page debugged

--- src/screens/ProductDetail/ProductDetailCard.jsx
+++ src/screens/ProductDetail/ProductDetailCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import img from "../../assets/img/product-1.jpg";
 
-const ProductDetailCard = () => {
+const ProductDetailCard = ({ selectedProduct }) => {
   const [quantity, setQuantity] = useState(0);
 
   const increaseQuantity = () => {
@@ -23,11 +23,11 @@ const ProductDetailCard = () => {
   return (
     <div className="flex justify-between px-28 bg-white py-10  rounded-3xl font-yekanReg">
       <div className="w-1/4">
-        <img className="w-full" src={img} alt="" />
+        <img className="w-full" src={selectedProduct.img} alt="" />
       </div>
       <div>
         <h1 className="text-xl font-yekanBold border-b-2 pb-4">
-          غذای خشک تیدا
+          {selectedProduct.title}
         </h1>
         <p className="flex items-center gap-1 mt-6">
           <svg
@@ -39,11 +39,11 @@ const ProductDetailCard = () => {
           >
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
           </svg>
-          <span>3</span>
+          <span>{selectedProduct.score}</span>
         </p>
         <p className="flex my-4 items-center gap-1">
           <span className="text-gray-400">موجودی:</span>
-          <span>44</span>
+          <span>{selectedProduct.stock}</span>
           <span>عدد در انبار</span>
         </p>
         <div className="flex flex-col gap-2">
@@ -55,7 +55,7 @@ const ProductDetailCard = () => {
           <div className="flex items-center gap-2">
             <p className="text-gray-400">قیمت واحد:</p>
             <p className="font-yekanBold text-[#FFAA00]">
-              {formatPrice(1000000)}
+              {formatPrice(selectedProduct.price)}
             </p>
             <p className="text-[#FFAA00]">تومان</p>
           </div>
